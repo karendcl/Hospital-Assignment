07610ad16bac82d11f28d2992e9effc147ff233f
hmrguez <zealot.algo@gmail.com>
2024-03-18 20:10:58 -0400

    Added stats

--- logic/endpoints.py
+++ logic/endpoints.py
@@ -28,13 +28,17 @@ async def root():
 @app.get("/start_simulation")
 async def start_simulation(icu_beds: int=10, common_beds: int=40):
     sim.start_simulation(icu_beds, common_beds)
-    print(sim.simulat)
     return {"message": "Simulation started"}
 
 
 @app.get("/day_statistics/{day}")
 async def day_statistics(day:int):
     return sim.get_day_statistics(day)
+
+
+
+
+
 
 
 
